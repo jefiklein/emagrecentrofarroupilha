23483ea1c2d9e15dda4ffaac9d0bf72ff9895f79
jefiklein <110074213+jefiklein@users.noreply.github.com>
2023-08-10 09:49:52 -0300

    Add files via upload

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,1 @@
-$(document).ready(function () {
-    $('.btn-contact').bind('click', function () {
-        var url = $(this).attr('href');
-        window.open(url, '_blank');
-    });
-});
+$(document).ready(function(){$(".btn-contact").bind("click",function(){var n=$(this).attr("href");window.open(n,"_blank")})});
